75cf7cf971b678234d997d4ba30ab31ebbb28bd8
Ignashin Konstantin <admin@trader.uz>
2012-10-21 14:46:11 +0500

    Signal delivery
    First signal delivery mailer

--- adm/classes/controller/api.php
+++ adm/classes/controller/api.php
@@ -16,5 +16,40 @@ class Controller_Api extends Controller {
 		}			
 		$this->response->body('{}');
 	}
+	
+	public function action_importsignal(){
+		$subscribers = Model_Api::getSubscribers();
+		$h = round($_GET['High'],4);
+		$l = round($_GET['Low'],4);
+		$tpl = '
+		<table>
+			<tr>							
+				<th>High</th>
+				<th>Low</th>
+				<th>TimeFrame</th>	
+			</tr>
+			<tr>							
+				<td>'.$h.'</td>
+				<td>'.$l.'</td>
+				<td>'.$_GET['timeFrame'].'</td>	
+			</tr>
+		</table>';
+		foreach($subscribers as $k=>$v){			
+			$this->send($v['email'], 'alerts@trader.uz', 'H:'.$h.', L:'.$l.', R:'.round((($h-$l)*10000),0).', T:'.$_GET['timeFrame'], $tpl);		
+		}
+		$this->response->body('1');
+	}
+	
+public static function send($to, $sender, $subject, $mess){	   
+	  $sendmail = "/usr/sbin/sendmail -t -f $sender "; 
+	  $fd = popen($sendmail, "w"); 
+	  fputs($fd, "To: ".$to."\r\n"); 
+	  fputs($fd, "Content-type: text/html; charset=utf-8 \r\n"); 
+	  fputs($fd, "From: Signal Trader.Uz <$sender>\r\n"); 
+	  fputs($fd, "Subject: ".$subject."\r\n"); 
+	  fputs($fd, "X-Mailer: Mailer Name\r\n\r\n"); 
+	  fputs($fd, $mess); 
+	  pclose($fd);		  	        
+	}
 
 } // End Welcome
--- adm/classes/model/api.php
+++ adm/classes/model/api.php
@@ -8,4 +8,10 @@ class Model_Api extends Kohana_Model {
 	public static function insertLoad($ip){
 		   return DB::query(Database::INSERT, "INSERT INTO ".self::$t." (tstamp,ip) VALUES ('".time()."','".$ip."')")->execute();	   	  	      
 	}
+	
+	public static function getSubscribers(){
+		return  DB::query(Database::SELECT, "SELECT * FROM api_users ORDER BY id DESC Limit 0, 20")->execute()->as_array();		   
+	
+	
+	}
 }
